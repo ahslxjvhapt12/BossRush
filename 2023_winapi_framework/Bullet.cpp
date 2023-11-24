b5a01872ef93ef53e853fb393bbd9a42e8df513d
ahslxjvhapt12 <likjhee1@gmail.com>
2023-11-24 11:37:05 +0900

    뭐지

--- 2023_winapi_framework/Bullet.cpp
+++ 2023_winapi_framework/Bullet.cpp
@@ -6,7 +6,7 @@
 Bullet::Bullet()
 //	: m_fDir(-1.f)
 	: m_fTheta(0.f)
-	, m_vDir(Vec2(0.f,0.f))
+	, m_vDir(Vec2(0.f, 0.f))
 	, m_pTex(nullptr)
 {
 	m_pTex = ResMgr::GetInst()->TexLoad(L"Bullet", L"Texture\\Bullet.bmp");
@@ -36,10 +36,6 @@ void Bullet::Render(HDC _dc)
 	int Width = m_pTex->GetWidth();
 	int Height = m_pTex->GetHeight();
 	//ELLIPSE_RENDER(vPos.x, vPos.y, vScale.x, vScale.y, _dc);
-	TransparentBlt(_dc
-		, (int)(vPos.x - vScale.x / 2)
-		, (int)(vPos.y - vScale.y / 2)
-		, Width, Height, m_pTex->GetDC()
-		, 0, 0, Width,Height, RGB(255,0,255));
+	TransparentBlt(_dc, (int)(vPos.x - vScale.x / 2), (int)(vPos.y - vScale.y / 2), Width, Height, m_pTex->GetDC(), 0, 0, Width, Height, RGB(255, 0, 255));
 	Component_Render(_dc);
 }
