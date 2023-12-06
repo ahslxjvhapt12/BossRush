707e3a4cf78a56bdb000bd403709e583d3127b5c
ahslxjvhapt12 <likjhee1@gmail.com>
2023-12-06 14:06:21 +0900

    FIx

---
+++ 2023_winapi_framework/EndingScene.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "EndingScene.h"
+
+void EndingScene::Init()
+{
+
+}
+
+void EndingScene::Update()
+{
+	Scene::Update();
+}
+
+void EndingScene::Render(HDC _dc)
+{
+	Scene::Render(_dc);
+}
+
+void EndingScene::Release()
+{
+	Scene::Release();
+}
